90d6f2b6000a67e9b6de54ed68562220690ee3a0
Gauthier Piarrette <gauthierpiarrette@gmail.com>
2026-09-17 09:20:59 +0200

    highball 0.9.23

--- Casks/highball.rb
+++ Casks/highball.rb
@@ -1,6 +1,6 @@
 cask "highball" do
-  version "0.9.19"
-  sha256 "42b2ec630f19a46be7027af4c994900f72a65c7aefa5c8d6fd5aaccbebaadd14"
+  version "0.9.23"
+  sha256 "58efaa2c3fad8cbaaab3670ca5149866b584a7e1245f95454c6c13c6f2031fd5"
 
   url "https://github.com/gauthierpiarrette/highball/releases/download/v#{version}/Highball.dmg"
   name "Highball"
